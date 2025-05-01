3d358912e6a4be9aabbfc8e7346f5382f159c887
muhzin ck <muhsinmuhsinmuhsi786@gmail.com>
2025-05-02 01:50:09 +0530

    cors changed

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8080;
 const app=express()
 
 app.use(cors({
-    origin: 'http://localhost:5173',  // frontend URL
+    origin: 'https://backend-wq7c.onrender.com',  
     credentials: true
   }));
 
